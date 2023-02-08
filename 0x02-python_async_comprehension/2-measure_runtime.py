26e8f906520635da441acc41d630f127d70dfd3a
mgmtsweni <mtswenigift@gmail.com>
2023-02-08 17:36:00 +0200

    change counter function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,8 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime():
     """measure the total runtime and return it"""
-    start_time = time.time()
+    start_time = time.perf_counter()
     measure = [async_comprehension() for i in range(4)]
     await asyncio.gather(*measure)
-    end_time = time.time()
+    end_time = time.perf_counter()
     return (end_time - start_time)
